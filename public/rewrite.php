596789f012684a4fa5b408da751b56b1492b4efa
Bob Fanger <bfanger@gmail.com>
2015-09-16 11:53:06 +0200

    SERVER_ADMIN heroku fix

--- public/rewrite.php
+++ public/rewrite.php
@@ -3,8 +3,10 @@
  * rewrite.php
  */
 define('Sledgehammer\STARTED', microtime(true));
+if (isset($_SERVER['HEROKU_ADMIN'])) {
+	$_SERVER['SERVER_ADMIN'] = $_SERVER['HEROKU_ADMIN'];
+}
 include(dirname(__FILE__).'/../vendor/sledgehammer/core/render_public_folders.php');
 require(dirname(__FILE__).'/../vendor/autoload.php');
-
 $app = new App();
 $app->handleRequest();
